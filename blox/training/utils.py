002623389142c023d3ffa83c3be028ce36304bf6
Łukasz Dudziak <l.dudziak@samsung.com>
2022-12-07 16:18:23 +0000

    Use standard RandAugment if possible (tested with PT 1.11)

--- blox/training/utils.py
+++ blox/training/utils.py
@@ -252,127 +252,130 @@ class Subset(torch.utils.data.Dataset):
         return len(self.indices)
 
 
-def _apply_op(img: torch.Tensor, op_name: str, magnitude: float,
-              interpolation: t.InterpolationMode, fill: Optional[List[float]]):
-    if op_name == "ShearX":
-        img = TVF.affine(img, angle=0.0, translate=[0, 0], scale=1.0, shear=[math.degrees(magnitude), 0.0],
-                       interpolation=interpolation, fill=fill)
-    elif op_name == "ShearY":
-        img = TVF.affine(img, angle=0.0, translate=[0, 0], scale=1.0, shear=[0.0, math.degrees(magnitude)],
-                       interpolation=interpolation, fill=fill)
-    elif op_name == "TranslateX":
-        img = TVF.affine(img, angle=0.0, translate=[int(magnitude), 0], scale=1.0,
-                       interpolation=interpolation, shear=[0.0, 0.0], fill=fill)
-    elif op_name == "TranslateY":
-        img = TVF.affine(img, angle=0.0, translate=[0, int(magnitude)], scale=1.0,
-                       interpolation=interpolation, shear=[0.0, 0.0], fill=fill)
-    elif op_name == "Rotate":
-        img = TVF.rotate(img, magnitude, interpolation=interpolation, fill=fill)
-    elif op_name == "Brightness":
-        img = TVF.adjust_brightness(img, 1.0 + magnitude)
-    elif op_name == "Color":
-        img = TVF.adjust_saturation(img, 1.0 + magnitude)
-    elif op_name == "Contrast":
-        img = TVF.adjust_contrast(img, 1.0 + magnitude)
-    elif op_name == "Sharpness":
-        img = TVF.adjust_sharpness(img, 1.0 + magnitude)
-    elif op_name == "Posterize":
-        img = TVF.posterize(img, int(magnitude))
-    elif op_name == "Solarize":
-        img = TVF.solarize(img, magnitude)
-    elif op_name == "AutoContrast":
-        img = TVF.autocontrast(img)
-    elif op_name == "Equalize":
-        img = TVF.equalize(img)
-    elif op_name == "Invert":
-        img = TVF.invert(img)
-    elif op_name == "Identity":
-        pass
-    else:
-        raise ValueError("The provided operator {} is not recognized.".format(op_name))
-    return img
-
-
-class RandAugment(torch.nn.Module):
-    r"""RandAugment data augmentation method based on
-    `"RandAugment: Practical automated data augmentation with a reduced search space"
-    <https://arxiv.org/abs/1909.13719>`_.
-    If the image is torch Tensor, it should be of type torch.uint8, and it is expected
-    to have [..., 1 or 3, H, W] shape, where ... means an arbitrary number of leading dimensions.
-    If img is PIL Image, it is expected to be in mode "L" or "RGB".
-
-    Args:
-        num_ops (int): Number of augmentation transformations to apply sequentially.
-        magnitude (int): Magnitude for all the transformations.
-        num_magnitude_bins (int): The number of different magnitude values.
-        interpolation (InterpolationMode): Desired interpolation enum defined by
-            :class:`torchvision.transforms.InterpolationMode`. Default is ``InterpolationMode.NEAREST``.
-            If input is Tensor, only ``InterpolationMode.NEAREST``, ``InterpolationMode.BILINEAR`` are supported.
-        fill (sequence or number, optional): Pixel fill value for the area outside the transformed
-            image. If given a number, the value is used for all bands respectively.
-        """
-
-    def __init__(self, num_ops: int = 2, magnitude: int = 9, num_magnitude_bins: int = 31,
-                 interpolation: t.InterpolationMode = t.InterpolationMode.NEAREST,
-                 fill: Optional[List[float]] = None) -> None:
-        super().__init__()
-        self.num_ops = num_ops
-        self.magnitude = magnitude
-        self.num_magnitude_bins = num_magnitude_bins
-        self.interpolation = interpolation
-        self.fill = fill
-
-    def _augmentation_space(self, num_bins: int, image_size: List[int]) -> Dict[str, Tuple[torch.Tensor, bool]]:
-        return {
-            # op_name: (magnitudes, signed)
-            "Identity": (torch.tensor(0.0), False),
-            "ShearX": (torch.linspace(0.0, 0.3, num_bins), True),
-            "ShearY": (torch.linspace(0.0, 0.3, num_bins), True),
-            "TranslateX": (torch.linspace(0.0, 150.0 / 331.0 * image_size[0], num_bins), True),
-            "TranslateY": (torch.linspace(0.0, 150.0 / 331.0 * image_size[1], num_bins), True),
-            "Rotate": (torch.linspace(0.0, 30.0, num_bins), True),
-            "Brightness": (torch.linspace(0.0, 0.9, num_bins), True),
-            "Color": (torch.linspace(0.0, 0.9, num_bins), True),
-            "Contrast": (torch.linspace(0.0, 0.9, num_bins), True),
-            "Sharpness": (torch.linspace(0.0, 0.9, num_bins), True),
-            "Posterize": (8 - (torch.arange(num_bins) / ((num_bins - 1) / 4)).round().int(), False),
-            "Solarize": (torch.linspace(256.0, 0.0, num_bins), False),
-            "AutoContrast": (torch.tensor(0.0), False),
-            "Equalize": (torch.tensor(0.0), False),
-        }
-
-    def forward(self, img: torch.Tensor) -> torch.Tensor:
-        """
-            img (PIL Image or Tensor): Image to be transformed.
-
-        Returns:
-            PIL Image or Tensor: Transformed image.
-        """
-        fill = self.fill
-        if isinstance(img, torch.Tensor):
-            if isinstance(fill, (int, float)):
-                fill = [float(fill)] * F.get_image_num_channels(img)
-            elif fill is not None:
-                fill = [float(f) for f in fill]
-
-        for _ in range(self.num_ops):
-            op_meta = self._augmentation_space(self.num_magnitude_bins, TVF._get_image_size(img))
-            op_index = int(torch.randint(len(op_meta), (1,)).item())
-            op_name = list(op_meta.keys())[op_index]
-            magnitudes, signed = op_meta[op_name]
-            magnitude = float(magnitudes[self.magnitude].item()) if magnitudes.ndim > 0 else 0.0
-            if signed and torch.randint(2, (1,)):
-                magnitude *= -1.0
-            img = _apply_op(img, op_name, magnitude, interpolation=self.interpolation, fill=fill)
-
+try:
+    RandAugment = t.RandAugment
+except AttributeError:
+    def _apply_op(img: torch.Tensor, op_name: str, magnitude: float,
+                interpolation: t.InterpolationMode, fill: Optional[List[float]]):
+        if op_name == "ShearX":
+            img = TVF.affine(img, angle=0.0, translate=[0, 0], scale=1.0, shear=[math.degrees(magnitude), 0.0],
+                        interpolation=interpolation, fill=fill)
+        elif op_name == "ShearY":
+            img = TVF.affine(img, angle=0.0, translate=[0, 0], scale=1.0, shear=[0.0, math.degrees(magnitude)],
+                        interpolation=interpolation, fill=fill)
+        elif op_name == "TranslateX":
+            img = TVF.affine(img, angle=0.0, translate=[int(magnitude), 0], scale=1.0,
+                        interpolation=interpolation, shear=[0.0, 0.0], fill=fill)
+        elif op_name == "TranslateY":
+            img = TVF.affine(img, angle=0.0, translate=[0, int(magnitude)], scale=1.0,
+                        interpolation=interpolation, shear=[0.0, 0.0], fill=fill)
+        elif op_name == "Rotate":
+            img = TVF.rotate(img, magnitude, interpolation=interpolation, fill=fill)
+        elif op_name == "Brightness":
+            img = TVF.adjust_brightness(img, 1.0 + magnitude)
+        elif op_name == "Color":
+            img = TVF.adjust_saturation(img, 1.0 + magnitude)
+        elif op_name == "Contrast":
+            img = TVF.adjust_contrast(img, 1.0 + magnitude)
+        elif op_name == "Sharpness":
+            img = TVF.adjust_sharpness(img, 1.0 + magnitude)
+        elif op_name == "Posterize":
+            img = TVF.posterize(img, int(magnitude))
+        elif op_name == "Solarize":
+            img = TVF.solarize(img, magnitude)
+        elif op_name == "AutoContrast":
+            img = TVF.autocontrast(img)
+        elif op_name == "Equalize":
+            img = TVF.equalize(img)
+        elif op_name == "Invert":
+            img = TVF.invert(img)
+        elif op_name == "Identity":
+            pass
+        else:
+            raise ValueError("The provided operator {} is not recognized.".format(op_name))
         return img
 
-    def __repr__(self) -> str:
-        s = self.__class__.__name__ + '('
-        s += 'num_ops={num_ops}'
-        s += ', magnitude={magnitude}'
-        s += ', num_magnitude_bins={num_magnitude_bins}'
-        s += ', interpolation={interpolation}'
-        s += ', fill={fill}'
-        s += ')'
-        return s.format(**self.__dict__)
+
+    class RandAugment(torch.nn.Module):
+        r"""RandAugment data augmentation method based on
+        `"RandAugment: Practical automated data augmentation with a reduced search space"
+        <https://arxiv.org/abs/1909.13719>`_.
+        If the image is torch Tensor, it should be of type torch.uint8, and it is expected
+        to have [..., 1 or 3, H, W] shape, where ... means an arbitrary number of leading dimensions.
+        If img is PIL Image, it is expected to be in mode "L" or "RGB".
+
+        Args:
+            num_ops (int): Number of augmentation transformations to apply sequentially.
+            magnitude (int): Magnitude for all the transformations.
+            num_magnitude_bins (int): The number of different magnitude values.
+            interpolation (InterpolationMode): Desired interpolation enum defined by
+                :class:`torchvision.transforms.InterpolationMode`. Default is ``InterpolationMode.NEAREST``.
+                If input is Tensor, only ``InterpolationMode.NEAREST``, ``InterpolationMode.BILINEAR`` are supported.
+            fill (sequence or number, optional): Pixel fill value for the area outside the transformed
+                image. If given a number, the value is used for all bands respectively.
+            """
+
+        def __init__(self, num_ops: int = 2, magnitude: int = 9, num_magnitude_bins: int = 31,
+                    interpolation: t.InterpolationMode = t.InterpolationMode.NEAREST,
+                    fill: Optional[List[float]] = None) -> None:
+            super().__init__()
+            self.num_ops = num_ops
+            self.magnitude = magnitude
+            self.num_magnitude_bins = num_magnitude_bins
+            self.interpolation = interpolation
+            self.fill = fill
+
+        def _augmentation_space(self, num_bins: int, image_size: List[int]) -> Dict[str, Tuple[torch.Tensor, bool]]:
+            return {
+                # op_name: (magnitudes, signed)
+                "Identity": (torch.tensor(0.0), False),
+                "ShearX": (torch.linspace(0.0, 0.3, num_bins), True),
+                "ShearY": (torch.linspace(0.0, 0.3, num_bins), True),
+                "TranslateX": (torch.linspace(0.0, 150.0 / 331.0 * image_size[0], num_bins), True),
+                "TranslateY": (torch.linspace(0.0, 150.0 / 331.0 * image_size[1], num_bins), True),
+                "Rotate": (torch.linspace(0.0, 30.0, num_bins), True),
+                "Brightness": (torch.linspace(0.0, 0.9, num_bins), True),
+                "Color": (torch.linspace(0.0, 0.9, num_bins), True),
+                "Contrast": (torch.linspace(0.0, 0.9, num_bins), True),
+                "Sharpness": (torch.linspace(0.0, 0.9, num_bins), True),
+                "Posterize": (8 - (torch.arange(num_bins) / ((num_bins - 1) / 4)).round().int(), False),
+                "Solarize": (torch.linspace(256.0, 0.0, num_bins), False),
+                "AutoContrast": (torch.tensor(0.0), False),
+                "Equalize": (torch.tensor(0.0), False),
+            }
+
+        def forward(self, img: torch.Tensor) -> torch.Tensor:
+            """
+                img (PIL Image or Tensor): Image to be transformed.
+
+            Returns:
+                PIL Image or Tensor: Transformed image.
+            """
+            fill = self.fill
+            if isinstance(img, torch.Tensor):
+                if isinstance(fill, (int, float)):
+                    fill = [float(fill)] * F.get_image_num_channels(img)
+                elif fill is not None:
+                    fill = [float(f) for f in fill]
+
+            for _ in range(self.num_ops):
+                op_meta = self._augmentation_space(self.num_magnitude_bins, TVF._get_image_size(img))
+                op_index = int(torch.randint(len(op_meta), (1,)).item())
+                op_name = list(op_meta.keys())[op_index]
+                magnitudes, signed = op_meta[op_name]
+                magnitude = float(magnitudes[self.magnitude].item()) if magnitudes.ndim > 0 else 0.0
+                if signed and torch.randint(2, (1,)):
+                    magnitude *= -1.0
+                img = _apply_op(img, op_name, magnitude, interpolation=self.interpolation, fill=fill)
+
+            return img
+
+        def __repr__(self) -> str:
+            s = self.__class__.__name__ + '('
+            s += 'num_ops={num_ops}'
+            s += ', magnitude={magnitude}'
+            s += ', num_magnitude_bins={num_magnitude_bins}'
+            s += ', interpolation={interpolation}'
+            s += ', fill={fill}'
+            s += ')'
+            return s.format(**self.__dict__)
